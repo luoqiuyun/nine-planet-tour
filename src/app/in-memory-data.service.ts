eabd606bd17ccdef74b2d9320220e82a82f6de89
luoqiuyun <qiuyun.luo@outlook.com>
2017-06-16 11:06:51 -0400

    update planet descriptions

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -2,16 +2,15 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     let planets = [
-      {id: 11, name: 'Mr. Nice'},
-      {id: 12, name: 'Narco'},
-      {id: 13, name: 'Bombasto'},
-      {id: 14, name: 'Celeritas'},
-      {id: 15, name: 'Magneta'},
-      {id: 16, name: 'RubberMan'},
-      {id: 17, name: 'Dynama'},
-      {id: 18, name: 'Dr IQ'},
-      {id: 19, name: 'Magma'},
-      {id: 20, name: 'Tornado'}
+      {id: 11, name: 'Mercury', desc: 'The Swiftest Planet'},
+      {id: 12, name: 'Venus', desc: 'Planetary Hot Spot'},
+      {id: 13, name: 'Earth', desc: 'Our Home Planet'},
+      {id: 14, name: 'Mars', desc: 'The Red Planet'},
+      {id: 15, name: 'Jupiter', desc: 'King of the Planets'},
+      {id: 16, name: 'Saturn', desc: 'Jewel of Our Solar System'},
+      {id: 17, name: 'Uranus', desc: 'The Sideways Planet'},
+      {id: 18, name: 'Neptune', desc: 'The Windiest Planet'},
+      {id: 19, name: 'Sun', desc: 'The largest object in the solar system'}
     ];
     return {planets};
   }
